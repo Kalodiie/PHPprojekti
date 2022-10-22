12363cdbc870aa7381c8f6a9f0e3bfbc5de4b66f
Kalodiie <Kalodiie@users.noreply.github.com>
2022-10-23 00:03:14 +0200

    Online narucivanje lekova
    Pomocu POST metode, iz HTML-a smo izvukli potrebne informacije kojima smo kasnije upravljali

---
+++ Apoteka/akcija.php
@@ -0,0 +1,39 @@
+<style>
+    <?php
+        include 'style.css';
+    ?>
+</style>
+<?php
+$aspirin = $_POST[ 'nAspirin' ] * 150;
+$kafetin = $_POST[ 'nKafetin' ] * 210;
+$panadol = $_POST[ 'nPanadol' ] * 290;
+$survey = $_POST[ 'nInfo' ];
+$cena = $aspirin + $kafetin + $panadol;
+define( 'porez', 1.08 );
+echo '<h2>Racun iz apoteke</h2><br>';
+echo '<table>
+<tr>
+    <th>Lek</th><th>Kolicina</th><th>Cena</th>
+</tr>';
+if ( $aspirin > 0 ) {
+    echo '<tr>
+        <td>Aspirin</td><td>'.$aspirin/150 .'</td><td>'.$aspirin.'</td>
+    </tr>';
+}
+if ( $kafetin > 0 ) {
+    echo '<tr>
+        <td>Kafetin</td><td>'.$kafetin/210 .'</td><td>'.$kafetin.'</td>
+    </tr>';
+}
+if ( $panadol > 0 ) {
+    echo '<tr>
+        <td>Panadol</td><td>'.$panadol/290 .'</td><td>'.$panadol.'</td>
+    </tr>';
+}
+
+echo '</table><br>
+<p>Ukupna cena:'.$cena.'</p><p>Cena sa dodatim porezom:'.$cena*porez.'</p>';
+
+echo '<button onclick="history.back()">Nazad na kupovinu</button>';
+?>
+
